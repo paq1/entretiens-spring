0d6138f4c47c9534eabd46250d27464e5dbb04b9
Pierre Paquin <p.paquin1016@laposte.net>
2024-05-26 20:53:31 +0200

    wip

--- app/src/main/java/org/example/app/api/HelloWorldController.java
+++ app/src/main/java/org/example/app/api/HelloWorldController.java
@@ -1,2 +1,13 @@
-package org.example.app.api;public class HelloWorldController {
+package org.example.app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
 }
