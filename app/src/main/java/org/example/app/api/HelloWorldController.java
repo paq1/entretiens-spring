de168e68ad74b255719745f98c50fb05656c1ade
Pierre Paquin <p.paquin1016@laposte.net>
2024-05-27 21:28:07 +0200

    wip add repo monogo

--- app/src/main/java/org/example/app/api/HelloWorldController.java
+++ app/src/main/java/org/example/app/api/HelloWorldController.java
@@ -1,5 +1,7 @@
 package org.example.app.api;
 
+import org.example.app.dbo.Todo;
+import org.example.app.repositories.TodoRepository;
 import org.example.core.services.HelloWorldService;
 import org.example.models.views.JsonApi;
 import org.springframework.http.HttpStatus;
@@ -7,17 +9,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class HelloWorldController {
 
     private final HelloWorldService helloWorldService;
+    private final TodoRepository todoRepository;
 
-    public HelloWorldController(HelloWorldService helloWorldService) {
+    public HelloWorldController(
+            HelloWorldService helloWorldService,
+            TodoRepository todoRepository
+    ) {
         this.helloWorldService = helloWorldService;
+        this.todoRepository = todoRepository;
     }
 
     @GetMapping("/")
     public ResponseEntity<JsonApi> hello() {
+
+        this.todoRepository.insert(new Todo(UUID.randomUUID().toString(), "test", "test", true));
+
         return new ResponseEntity<>(new JsonApi(this.helloWorldService.sayHello()), HttpStatus.OK);
     }
 }
